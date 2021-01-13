5c5ffa1e6b071aa608cb3e9236ca2dd98e4ad418
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2021-01-13 11:14:26 -0300

    Adiciona as rotas de navegaçao entre as paginas

---
+++ src/routes/index.js
@@ -0,0 +1,19 @@
+import React from 'react';
+import { Route, Switch } from 'react-router-dom';
+import Index from '../pages/Index';
+import Login from '../pages/Customer/Login';
+import Main from '../pages/Customer/Main';
+import Error from '../pages/Error';
+
+const Routes = () => {
+    return (
+        <Switch>
+            <Route exact path="/" component={Index}/>
+            <Route exact path="/acessar/:id" component={Login}/>
+            <Route exact path="/main" component={Login}/>
+            <Route path="*" component={Error}/>
+        </Switch>
+    )
+};
+
+export default Routes;
