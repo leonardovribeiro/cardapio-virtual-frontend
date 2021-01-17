3438cfb5a25a18010f3a8ad03276a7f540235812
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2021-01-17 12:19:08 -0300

    Adicionei o componente VerticalScrollView

---
+++ src/components/VerticalScrollView/index.js
@@ -0,0 +1,31 @@
+import React, { Component } from 'react';
+import { Category, Container } from './styles';
+
+class VerticalScrollView extends Component {
+    render(){
+        return(
+            <Container>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+                <Category>
+                    <img src="https://images2.minutemediacdn.com/image/upload/c_crop,h_1126,w_2000,x_0,y_181/f_auto,q_auto,w_1100/v1554932288/shape/mentalfloss/12531-istock-637790866.jpg" alt="food"/>
+                </Category>
+            </Container>
+        )
+    }
+}
+
+export default VerticalScrollView;
---
+++ src/components/VerticalScrollView/styles.js
@@ -0,0 +1,30 @@
+import styled from 'styled-components';
+
+export const Container = styled.div`
+   display: flex;
+   flex-direction: column;
+   align-items: center;
+   overflow-y: auto;
+
+   &::-webkit-scrollbar {
+        width: 0;
+    }
+`;
+
+export const Category = styled.div`
+    max-width: 250px;
+    border: 1px solid #ddd;
+    border-radius: 5px;
+    justify-content: center;
+    align-items: center;
+
+    img {
+        width: 100%;
+    }
+
+    &:first-child{
+        margin-top: 10px;
+    }
+
+    margin-bottom: 10px;
+`;
