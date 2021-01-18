45d371bb3497437d3f0731b677578cd86f0a0b4b
Leonardo Victor Ribeiro <leonardovictor@outlook.com>
2021-01-18 13:18:38 -0300

    Adicionei a pagina que realiza o fechamento da comanda

--- src/pages/Customer/Comanda/index.js
+++ src/pages/Customer/Comanda/index.js
@@ -1,18 +1,52 @@
-import React, { Component } from 'react';
-import { Container } from './styles';
+import React from 'react';
+import { Container, Wrapper, Content, Box, Button } from './styles';
 import Navigation from '../../../components/Navigation';
 import Header from '../../../components/Header';
+import { useHistory } from 'react-router-dom';
 
-class Comanda extends Component {
-    render(){
-        return (
-            <Container>
-                <Header />
-                <h1>Comanda</h1>
-                <Navigation/>
-            </Container>
-        )
-    }
+const Comanda = () => {
+    const history = useHistory();
+    return (
+        <Container>
+            <Header />
+            <Wrapper>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Content>
+                    <span>#20200001</span>
+                    <span>R$ 100,00</span>
+                </Content>
+                <Box>
+                    <span>R$ 500,00</span>
+                </Box>
+  
+            </Wrapper>
+            <Button onClick={() => history.push('/payment')}>Ir para o pagamento</Button>
+            <Navigation/>
+        </Container>
+    )
 }
 
 export default Comanda;
--- src/pages/Customer/Comanda/styles.js
+++ src/pages/Customer/Comanda/styles.js
@@ -1,8 +1,61 @@
 import styled from 'styled-components';
+import { shade } from 'polished';
 
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
     justify-content: space-between;
     height: 100vh;
+
+`;
+
+export const Wrapper = styled.div`
+    flex: 1;
+    padding: 20px;
+    overflow-y: auto;
+
+    &::-webkit-scrollbar {
+        width: 0;
+    }
+`;
+
+export const Content = styled.div`
+    font-size: 18px;
+    display: flex;
+    flex-direction: row;
+    justify-content: space-between;
+    border: 1px solid #dedede;
+    border-radius: 5px;
+    padding: 20px;
+    margin-bottom: 10px;
+
+    span {
+        font-weight: bold;
+    }
+`;
+
+export const Box = styled.div`
+    display: flex;
+    flex-direction: row;
+    justify-content: flex-end;
+
+    span {
+        font-size: 24px;
+    }
+`;
+
+export const Button = styled.button`
+    background: #C8161D;
+    height: 50px;
+    padding: 18px;
+    border-radius: 5px;
+    border: 0;
+    color: #FFFFFF;
+    font-weight: bold;
+    transition: background-color 0.2s;
+    margin: 20px;
+
+    &:hover{
+        background: ${shade(0.2, '#C8161D')}
+    }
 `;
