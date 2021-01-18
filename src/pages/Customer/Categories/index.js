f0f3a7c57e488e0c5ea73a7149d018f963a90bf4
mararbia <mararbiaoliveira@gmail.com>
2021-01-17 23:07:53 -0300

    Commit para fins de Merge com a Main

--- src/pages/Customer/Categories/index.js
+++ src/pages/Customer/Categories/index.js
@@ -2,13 +2,26 @@ import React, { Component } from 'react';
 import { Container } from './styles';
 import Navigation from '../../../components/Navigation';
 import Header from '../../../components/Header';
+import {styled} from 'styled-components';
 
 class Categories extends Component {
     render(){
         return (
             <Container>
                 <Header />
-                <h1>Categorias</h1>
+                <ul>
+                    <li className='img-principal1'>black</li>
+                    <li className='img-food1'>red</li>
+                    <li className='img-food2'>red</li>
+                </ul>
+                <ul>
+                    <li className='img-principal2'>black</li>
+                    <li className='img-food3'>red</li>
+                    <li className='img-food4'>red</li>
+                </ul>
+
+
+                
                 <Navigation/>
             </Container>
         )
--- src/pages/Customer/Categories/styles.js
+++ src/pages/Customer/Categories/styles.js
@@ -3,6 +3,75 @@ import styled from 'styled-components';
 export const Container = styled.div`
     display: flex;
     flex-direction: column;
-    justify-content: space-between;
+    //justify-content: space-between;
     height: 100vh;
+
+    .img-principal1{
+        background-color: black;
+        color: white;
+        display: flex;
+        margin-bottom: 100%;
+        height: 40vh;
+
+        //width: 100%;
+        //margin: 0 0 20em;  
+        //flex-direction: column;
+        //flex-wrap: wrap;
+           
+    }
+
+    .img-food1{
+        background-color: red;
+        display: flex;
+        margin-bottom: 100%;
+        height: 30vh;
+
+        //width: 100%;
+        
+    }
+
+    .img-food2{
+        background-color: pink;
+        display: flex;
+        margin-bottom: 100%;
+        height: 30vh;
+
+        //width: 100%;
+        
+    }
+
+    .img-principal2{
+        background-color: black;
+        color: white;
+        display: flex;
+        margin-bottom: 100%;
+        height: 40vh;
+
+        //width: 100%;
+        //margin: 0 0 20em;  
+        //flex-direction: column;
+        //flex-wrap: wrap;
+           
+    }
+
+    .img-food3{
+        background-color: red;
+        display: flex;
+        margin-bottom: 100%;
+        height: 30vh;
+
+        //width: 100%;
+        
+    }
+
+    .img-food4{
+        background-color: pink;
+        display: flex;
+        margin-bottom: 100%;
+        height: 30vh;
+
+        //width: 100%;
+        
+    }
 `;
+
